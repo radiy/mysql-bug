19385ad2a50a2885b7e40a9647863513f2a2a127
Квасов Роман <r.kvasov@analit.net>
2018-06-04 11:59:21 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace mysql_bug
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var connection = new MySqlConnection("server=localhost;SslMode=none;user=root")) {
+				connection.Open();
+
+				var setup = @"
+
+drop database if exists mysqlbug;
+create database mysqlbug;
+use mysqlbug;
+
+
+CREATE PROCEDURE Test ()
+BEGIN
+	SELECT 1;
+END
+";
+
+
+				var cmd = new MySqlCommand();
+				cmd.Connection = connection;
+				cmd.CommandText = setup;
+				cmd.ExecuteNonQuery();
+
+
+				cmd.CommandText = "Mysqlbug.Test";
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.ExecuteNonQuery();
+			}
+		}
+	}
+}
